169e22713b8d66d08783e4f2bf6ff71bae7e297a
Say a Wan <sayawan.devs@gmail.com>
2015-05-19 14:04:57 +0300

    content animation

--- app/client/views/layout/AppView.js
+++ app/client/views/layout/AppView.js
@@ -3,6 +3,8 @@ var Surface            = famous.core.Surface;
 var Transform          = famous.core.Transform;
 var StateModifier      = famous.modifiers.StateModifier;
 // var EventHandler       = famous.core.EventHandler;
+var Easing             = famous.transitions.Easing;
+// var Lightbox           = famous.views.Lightbox;
 var RenderController   = famous.views.RenderController;
 var HeaderFooterLayout = famous.views.HeaderFooterLayout;
 
@@ -46,6 +48,10 @@ AppView = function() {
     this.layout.footer.add(navbar.state).add(navbar);
 
     // content
+    // Comment out this line
+    // and make animation work
+    // disabling for now
+    // this.content = new Lightbox(this.options.transition);
     this.content = new RenderController();
     this.content.state = new StateModifier({
         // transform: Transform.behind
@@ -63,6 +69,21 @@ AppView.prototype = Object.create(View.prototype);
 AppView.prototype.constructor = AppView;
 
 AppView.DEFAULT_OPTIONS = {
+    transition: {
+        inTransform: Transform.translate(500, 0, -800),
+        outTransform: Transform.thenMove(Transform.rotateY(1.8), [-200, 0, -500]),
+        inTransition: { curve: Easing.outExpo, duration: 350},
+        outTransition: { curve: Easing.inQuad, duration: 250},
+        inOrigin: [0.5, 0],
+        inAlign: [0.5, 0],
+        outOrigin: [0.5, 0],
+        outAlign: [0.5, 0],
+        showOrigin: [0.5, 0],
+        showAlign: [0.5, 0],
+        inOpacity: 0,
+        outOpacity: 0,
+        overlap: false
+    }
 };
 
 AppView.prototype.getPage = function() {
